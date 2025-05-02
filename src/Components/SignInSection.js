0732b0bbbd0b7b6d0bdd9599f6cb560c96b03fa8
Rana Mubashir Tariq <mubashirtariq.1366@gmail.com>
2025-05-02 15:37:16 +0500

    add all files

--- src/Components/SignInSection.js
+++ src/Components/SignInSection.js
@@ -1,15 +1,28 @@
 import React , {useState} from 'react'
 import './SignInSection.css'
 import { Link , useNavigate  } from 'react-router-dom';
+import {signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../Firebase";
 
 export default function SignInSection() {
   const navigate = useNavigate();
 
-  const handleNavigate = () =>{
-    navigate('/dashboard');
-  }
-
     const [showPassword , setShowPassword] = useState(false);
+    const [email , setEmail] = useState('');
+    const [password , setPassword] =useState('')
+
+
+    const signIn = async () => {
+      try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        console.log("User signed in:", userCredential.user);
+          navigate('/dashboard');
+        alert("Sign in successful! 🎉");
+      } catch (error) {
+        console.error("Error signing in:", error.message);
+        alert(error.message);  // Show error to user
+      }
+    };
 
   return (
     <div className='sign-in-container'>
@@ -30,17 +43,17 @@ export default function SignInSection() {
 
                   <div className='input-section-div'>
                     <label>Email</label><br/>
-                     <input type="email" placeholder='Enter your email' /><br/>
+                     <input  type="email" placeholder='Enter your email' onChange={(e)=>setEmail(e.target.value)} /><br/>
                      <label>Password</label><br/>
                      <div className='icon-d'>
-                            <input onChange={(e)=>setShowPassword(e.target.value)} type={showPassword ? "text" : "password"} placeholder='Enter Password...'/>
+                            <input onChange={(e)=>setPassword(e.target.value)} type={showPassword ? "text" : "password"}  placeholder='Enter Password...'/>
                                     <div className="toggle-btn" onClick={() => setShowPassword(!showPassword)}>
                                                  {showPassword ? <i class="fa-solid fa-eye"/> : <i class="fa-solid fa-eye-slash"/>} 
                                     </div>
                          </div>
                   </div>
                    
-                <button className='sign-in-btn' onClick={handleNavigate}><Link>Sign In</Link></button>
+                <button className='sign-in-btn' onClick={signIn }><Link>Sign In</Link></button>
 
             </div>
             </div>
